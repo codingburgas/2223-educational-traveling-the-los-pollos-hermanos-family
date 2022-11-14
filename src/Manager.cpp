399a937ce18edca37383adbbde48e96bce9b56b3
DNKolev20 <DNKolev20@codingburgas.bg>
2022-11-14 20:55:40 +0200

    Create test button

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,5 +1,7 @@
 #include "Manager.hpp"
 
+#define NUM_FRAMES  3 
+
 bool shouldExit = false;
 
 void Manager::Initialize()
@@ -7,47 +9,45 @@ void Manager::Initialize()
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Europe");
 
     SetTargetFPS(60);
+
+    SetWindowSize(GetMonitorWidth(0), GetMonitorHeight(0));
+
+    ToggleFullscreen();
 }
 
 void Manager::Update()
 {
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // check for enter
- 		if (IsKeyPressed(KEY_ENTER))
- 		{
-            // see what display we are on right now
- 			int display = GetCurrentMonitor();
- 
-            
-            if (IsWindowFullscreen())
-            {
-                // if we are full screen, then go back to the windowed size
-                SetWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-            }
-            else
-            {
-                // if we are not full screen, set the window size to match the monitor we are on
-                SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
-            }
- 
-            // toggle the state
- 			ToggleFullscreen();
- 	    }
+    Texture2D texture = LoadTexture("assets/images/europe.png");
+    Texture2D button = LoadTexture("assets/images/france.png");
 
-        Image image = LoadImage("images/europe.png");     
-        Texture2D texture = LoadTextureFromImage(image);          
-        UnloadImage(image);
+    float frameHeight = (float)button.height/NUM_FRAMES;
+    Rectangle sourceRec = { 0, 0, (float)button.width, frameHeight };
 
-        BeginDrawing();
+    Rectangle btnBounds = { SCREEN_WIDTH/2.0f - button.width/2.0f, SCREEN_HEIGHT/2.0f - button.height/NUM_FRAMES/2.0f, (float)button.width, frameHeight };
 
-        ClearBackground(RAYWHITE);
+    int btnState = 0;
+
+    Vector2 mousePoint = { 0.0f, 0.0f };
+
+    mousePoint = GetMousePosition();
+
+        // Check button state
+        if (CheckCollisionPointRec(mousePoint, btnBounds))
+        {
+        }
+        else btnState = 0;
+
+    sourceRec.y = btnState*frameHeight;
+
+    BeginDrawing();
+
+        ClearBackground(RED);
 
         DrawTexture(texture, -120, 0, WHITE);
 
-        EndDrawing();
-    }
-    CloseWindow();
+        DrawTextureRec(button, sourceRec, {btnBounds.x, btnBounds.y }, WHITE);
+
+    EndDrawing();
 }   
 
 bool Manager::ShouldExit()
