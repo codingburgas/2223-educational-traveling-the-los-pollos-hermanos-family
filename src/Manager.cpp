84c12da775e351574b289e959b08f36a10750490
DNKolev20 <DNKolev20@codingburgas.bg>
2022-11-16 19:17:07 +0200

    Add button variables

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -17,16 +17,34 @@ void Manager::Update()
 {
     Texture2D map = LoadTexture("assets/images/europe.png");
 
-    Texture2D buttonfrance = LoadTexture("assets/images/france.png");
-    Texture2D buttonbulgaria = LoadTexture("assets/images/bulgaria.png");
-    
-    float frameHeightfrance = (float)buttonfrance.height/NUM_FRAMES;
-    Rectangle sourceRecfrance = { 0, 0, (float)buttonfrance.width, 100 };
-    float frameHeightbulgaria = (float)buttonbulgaria.height/NUM_FRAMES;
-    Rectangle sourceRecbulgaria = { 0, 0, (float)buttonbulgaria.width, 200 };
+    Texture2D buttonAustria = LoadTexture("assets/images/austria.png");
+    Texture2D buttonBelgium = LoadTexture("assets/images/belgium.png");
+    Texture2D buttonBulgaria = LoadTexture("assets/images/bulgaria.png");
+    Texture2D buttonDenmark = LoadTexture("assets/images/denmark.png");
+    Texture2D buttonFrance = LoadTexture("assets/images/france.png");
+    Texture2D buttonGermany = LoadTexture("assets/images/germany.png");
+    Texture2D buttonGreece = LoadTexture("assets/images/greece.png");
+    Texture2D buttonItaly = LoadTexture("assets/images/italy.png");
+    Texture2D buttonNetherlands = LoadTexture("assets/images/netherlands.png");
+    Texture2D buttonPoland = LoadTexture("assets/images/poland.png");
+    Texture2D buttonPortugal = LoadTexture("assets/images/portugal.png");
+    Texture2D buttonRomania = LoadTexture("assets/images/romania.png");
+    Texture2D buttonRussia = LoadTexture("assets/images/russia.png");
+    Texture2D buttonSerbia = LoadTexture("assets/images/serbia.png");
+    Texture2D buttonSweden = LoadTexture("assets/images/sweden.png");
+    Texture2D buttonSwitzerland = LoadTexture("assets/images/switzerland.png");
+    Texture2D buttonTurkey = LoadTexture("assets/images/turkey.png");
+    Texture2D buttonUk = LoadTexture("assets/images/uk.png");
+    Texture2D buttonUkraine = LoadTexture("assets/images/ukraine.png");
 
-    Rectangle btnBoundsfrance = { 480, 675, (float)buttonfrance.width, frameHeightfrance };
-    Rectangle btnBoundsbulgaria = { 100, 200, (float)buttonbulgaria.width, frameHeightbulgaria };
+    float frameHeightFrance = (float)buttonFrance.height/NUM_FRAMES;
+    Rectangle sourceRecFrance = { 0, 0, (float)buttonFrance.width, 100 };
+
+    float frameHeightBulgaria = (float)buttonBulgaria.height/NUM_FRAMES;
+    Rectangle sourceRecBulgaria = { 0, 0, (float)buttonBulgaria.width, 200 };
+
+    Rectangle btnBoundsFrance = { 480, 675, (float)buttonFrance.width, frameHeightFrance };
+    Rectangle btnBoundsBulgaria = { 100, 200, (float)buttonBulgaria.width, frameHeightBulgaria };
 
     int btnState = 0;
 
@@ -34,12 +52,12 @@ void Manager::Update()
 
     mousePoint = GetMousePosition();
 
-        if (CheckCollisionPointRec(mousePoint, btnBoundsfrance))
+        if (CheckCollisionPointRec(mousePoint, btnBoundsFrance))
         {
         }
         else btnState = 0;
 
-    sourceRecfrance.y = btnState*frameHeightfrance;
+    sourceRecFrance.y = btnState*frameHeightFrance;
 
     BeginDrawing();
 
@@ -47,8 +65,8 @@ void Manager::Update()
 
         DrawTexture(map, 0, 0, WHITE);
 
-        DrawTextureRec(buttonfrance, sourceRecfrance, {btnBoundsfrance.x, btnBoundsfrance.y }, WHITE);
-        // DrawTextureRec(buttonbulgaria, sourceRecbulgaria, {btnBoundsbulgaria.x, btnBoundsbulgaria.y }, WHITE); 
+        DrawTextureRec(buttonFrance, sourceRecFrance, {btnBoundsFrance.x, btnBoundsFrance.y }, WHITE);
+        DrawTextureRec(buttonBulgaria, sourceRecBulgaria, {btnBoundsBulgaria.x, btnBoundsBulgaria.y }, WHITE); 
 
     EndDrawing();
 }   
