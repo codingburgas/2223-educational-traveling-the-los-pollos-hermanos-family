a48841a9925519240270a8e0ce639043339e9db7
DNKolev20 <DNKolev20@codingburgas.bg>
2022-11-19 16:41:41 +0200

    Create button action

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,142 +1,195 @@
 #include "Manager.hpp"
 
+#include <iostream>
+#include <vector>
+
 #define NUM_FRAMES  3 
 
 bool shouldExit = false;
+
+Texture2D map;
+
+Texture2D buttonAustria;
+Texture2D buttonBelgium;
+Texture2D buttonBulgaria;
+Texture2D buttonDenmark;
+Texture2D buttonFrance;
+Texture2D buttonGermany;
+Texture2D buttonGreece;
+Texture2D buttonItaly;
+Texture2D buttonNetherlands;
+Texture2D buttonNorway;
+Texture2D buttonPoland;
+Texture2D buttonPortugal;
+Texture2D buttonRomania;
+Texture2D buttonRussia;
+Texture2D buttonSerbia;
+Texture2D buttonSweden;
+Texture2D buttonSwitzerland;
+Texture2D buttonTurkey;
+Texture2D buttonUK;
+Texture2D buttonUkraine;
+
+bool drawRec = false;
+int buttonToDisplay;
 
 void Manager::Initialize()
 {
     InitWindow(1080, 720, "Europe");
 
     SetTargetFPS(60);
+
+    map = LoadTexture("assets/images/europe.png");
+
+    buttonAustria = LoadTexture("assets/images/austria.png");
+    buttonBelgium = LoadTexture("assets/images/belgium.png");
+    buttonBulgaria = LoadTexture("assets/images/bulgaria.png");
+    buttonDenmark = LoadTexture("assets/images/denmark 2.png");
+    buttonFrance = LoadTexture("assets/images/france.png");
+    buttonGermany = LoadTexture("assets/images/germany.png");
+    buttonGreece = LoadTexture("assets/images/greece.png");
+    buttonItaly = LoadTexture("assets/images/italy.png");
+    buttonNetherlands = LoadTexture("assets/images/netherlands.png");
+    buttonNorway = LoadTexture("assets/images/norway.png");
+    buttonPoland = LoadTexture("assets/images/poland.png");
+    buttonPortugal = LoadTexture("assets/images/portugal.png");
+    buttonRomania = LoadTexture("assets/images/romania.png");
+    buttonRussia = LoadTexture("assets/images/russia.png");
+    buttonSerbia = LoadTexture("assets/images/serbia.png");
+    buttonSweden = LoadTexture("assets/images/sweden.png");
+    buttonSwitzerland = LoadTexture("assets/images/switzerland.png");
+    buttonTurkey = LoadTexture("assets/images/turkey.png");
+    buttonUK = LoadTexture("assets/images/uk.png");
+    buttonUkraine = LoadTexture("assets/images/ukraine.png");
 }
 
 void Manager::Update()
 {
-    Texture2D map = LoadTexture("assets/images/europe.png");
-
-    Texture2D buttonAustria = LoadTexture("assets/images/austria.png");
-    Texture2D buttonBelgium = LoadTexture("assets/images/belgium.png");
-    Texture2D buttonBulgaria = LoadTexture("assets/images/bulgaria.png");
-    Texture2D buttonDenmark = LoadTexture("assets/images/denmark.png");
-    Texture2D buttonFrance = LoadTexture("assets/images/france.png");
-    Texture2D buttonGermany = LoadTexture("assets/images/germany.png");
-    Texture2D buttonGreece = LoadTexture("assets/images/greece.png");
-    Texture2D buttonItaly = LoadTexture("assets/images/italy.png");
-    Texture2D buttonNetherlands = LoadTexture("assets/images/netherlands.png");
-    Texture2D buttonNorway = LoadTexture("assets/images/norway.png");
-    Texture2D buttonPoland = LoadTexture("assets/images/poland.png");
-    Texture2D buttonPortugal = LoadTexture("assets/images/portugal.png");
-    Texture2D buttonRomania = LoadTexture("assets/images/romania.png");
-    Texture2D buttonRussia = LoadTexture("assets/images/russia.png");
-    Texture2D buttonSerbia = LoadTexture("assets/images/serbia.png");
-    Texture2D buttonSweden = LoadTexture("assets/images/sweden.png");
-    Texture2D buttonSwitzerland = LoadTexture("assets/images/switzerland.png");
-    Texture2D buttonTurkey = LoadTexture("assets/images/turkey.png");
-    Texture2D buttonUK = LoadTexture("assets/images/uk.png");
-    Texture2D buttonUkraine = LoadTexture("assets/images/ukraine.png");
+    std::vector<Rectangle> buttons;
 
     int frameHeightAustria = buttonAustria.height/NUM_FRAMES;
     Rectangle sourceRecAustria = { 0, 0, (float)buttonAustria.width, 20 };
     Rectangle btnBoundsAustria = { 415, 445, (float)buttonAustria.width, (float)frameHeightAustria };
+    buttons.push_back(btnBoundsAustria);
 
     int frameHeightBelgium = buttonBelgium.height/NUM_FRAMES;
-    Rectangle sourceRecBelgium = { 0, 0, (float)buttonBelgium.width, 20 };
-    Rectangle btnBoundsBelgium = { 180, 450, (float)buttonBelgium.width, (float)frameHeightBelgium };
+    Rectangle sourceRecBelgium = { 0, 0, (float)buttonBelgium.width, 30 };
+    Rectangle btnBoundsBelgium = { 250, 380, (float)buttonBelgium.width, (float)frameHeightBelgium };
+    buttons.push_back(btnBoundsBelgium);
 
     int frameHeightBulgaria = buttonBulgaria.height/NUM_FRAMES;
-    Rectangle sourceRecBulgaria = { 0, 0, (float)buttonBulgaria.width, 200 };
-    Rectangle btnBoundsBulgaria = { 100, 200, (float)buttonBulgaria.width, (float)frameHeightBulgaria };
+    Rectangle sourceRecBulgaria = { 0, 0, (float)buttonBulgaria.width, 25 };
+    Rectangle btnBoundsBulgaria = { 590, 540, (float)buttonBulgaria.width, (float)frameHeightBulgaria };
+    buttons.push_back(btnBoundsBulgaria);
 
     int frameHeightDenmark = buttonDenmark.height/NUM_FRAMES;
-    Rectangle sourceRecDenmark = { 0, 0, (float)buttonDenmark.width, 100 };
-    Rectangle btnBoundsDenmark = { 480, 300, (float)buttonDenmark.width, (float)frameHeightDenmark };
+    Rectangle sourceRecDenmark = { 0, 0, (float)buttonDenmark.width, 40 };
+    Rectangle btnBoundsDenmark = { 350, 260, (float)buttonDenmark.width, (float)frameHeightDenmark };
+    buttons.push_back(btnBoundsDenmark);
 
     int frameHeightFrance = buttonFrance.height/NUM_FRAMES;
     Rectangle sourceRecFrance = { 0, 0, (float)buttonFrance.width, 50 };
     Rectangle btnBoundsFrance = { 180, 450, (float)buttonFrance.width, (float)frameHeightFrance };
+    buttons.push_back(btnBoundsFrance);
 
     int frameHeightGermany = buttonGermany.height/NUM_FRAMES;
     Rectangle sourceRecGermany = { 0, 0, (float)buttonGermany.width, 100 };
     Rectangle btnBoundsGermany = { 480, 675, (float)buttonGermany.width, (float)frameHeightGermany };
+    buttons.push_back(btnBoundsGermany);
 
     int frameHeightGreece = buttonGreece.height/NUM_FRAMES;
     Rectangle sourceRecGreece = { 0, 0, (float)buttonGreece.width, 100 };
     Rectangle btnBoundsGreece = { 400, 675, (float)buttonGreece.width, (float)frameHeightGreece };
+    buttons.push_back(btnBoundsGreece);
 
     int frameHeightItaly = buttonItaly.height/NUM_FRAMES;
     Rectangle sourceRecItaly = { 0, 0, (float)buttonItaly.width, 100 };
     Rectangle btnBoundsItaly = { 480, 675, (float)buttonItaly.width, (float)frameHeightItaly };
+    buttons.push_back(btnBoundsItaly);
 
     int frameHeightNetherlands = buttonNetherlands.height/NUM_FRAMES;
     Rectangle sourceRecNetherlands = { 0, 0, (float)buttonNetherlands.width, 100 };
     Rectangle btnBoundsNetherlands = { 480, 675, (float)buttonNetherlands.width, (float)frameHeightNetherlands };
+    buttons.push_back(btnBoundsNetherlands);
 
     int frameHeightNorway = buttonNorway.height/NUM_FRAMES;
     Rectangle sourceRecNorway = { 0, 0, (float)buttonNorway.width, 100 };
     Rectangle btnBoundsNorway = { 480, 675, (float)buttonNorway.width, (float)frameHeightNorway };
+    buttons.push_back(btnBoundsNorway);
 
     int frameHeightPoland = buttonPoland.height/NUM_FRAMES;
     Rectangle sourceRecPoland = { 0, 0, (float)buttonPoland.width, 100 };
     Rectangle btnBoundsPoland = { 480, 675, (float)buttonPoland.width, (float)frameHeightPoland };
+    buttons.push_back(btnBoundsPoland);
 
     int frameHeightPortugal = buttonPortugal.height/NUM_FRAMES;
     Rectangle sourceRecPortugal = { 0, 0, (float)buttonPortugal.width, 100 };
     Rectangle btnBoundsPortugal = { 480, 675, (float)buttonPortugal.width, (float)frameHeightPortugal };
+    buttons.push_back(btnBoundsPortugal);
 
     int frameHeightRomania = buttonRomania.height/NUM_FRAMES;
     Rectangle sourceRecRomania = { 0, 0, (float)buttonRomania.width, 100 };
     Rectangle btnBoundsRomania = { 480, 675, (float)buttonRomania.width, (float)frameHeightRomania };
+    buttons.push_back(btnBoundsRomania);
 
     int frameHeightRussia = buttonRussia.height/NUM_FRAMES;
     Rectangle sourceRecRussia = { 0, 0, (float)buttonRussia.width, 100 };
     Rectangle btnBoundsRussia = { 480, 675, (float)buttonRussia.width, (float)frameHeightRussia };
+    buttons.push_back(btnBoundsRussia);
 
     int frameHeightSerbia = buttonSerbia.height/NUM_FRAMES;
     Rectangle sourceRecSerbia = { 0, 0, (float)buttonSerbia.width, 100 };
     Rectangle btnBoundsSerbia = { 480, 675, (float)buttonSerbia.width, (float)frameHeightSerbia };
+    buttons.push_back(btnBoundsSerbia);
 
     int frameHeightSweden = buttonSweden.height/NUM_FRAMES;
     Rectangle sourceRecSweden = { 0, 0, (float)buttonSweden.width, 100 };
     Rectangle btnBoundsSweden = { 480, 675, (float)buttonSweden.width, (float)frameHeightSweden };
+    buttons.push_back(btnBoundsSweden);
 
     int frameHeightSwitzerland = buttonSwitzerland.height/NUM_FRAMES;
     Rectangle sourceRecSwitzerland = { 0, 0, (float)buttonSwitzerland.width, 100 };
     Rectangle btnBoundsSwitzerland = { 480, 675, (float)buttonSwitzerland.width, (float)frameHeightSwitzerland };
+    buttons.push_back(btnBoundsSwitzerland);
 
     int frameHeightTurkey = buttonTurkey.height/NUM_FRAMES;
     Rectangle sourceRecTurkey = { 0, 0, (float)buttonTurkey.width, 100 };
     Rectangle btnBoundsTurkey = { 480, 675, (float)buttonTurkey.width, (float)frameHeightTurkey };
+    buttons.push_back(btnBoundsTurkey);
 
     int frameHeightUK = buttonUK.height/NUM_FRAMES;
     Rectangle sourceRecUK = { 0, 0, (float)buttonUK.width, 100 };
     Rectangle btnBoundsUK = { 480, 675, (float)buttonUK.width, (float)frameHeightUK };
+    buttons.push_back(btnBoundsUK);
 
     int frameHeightUkraine = buttonUkraine.height/NUM_FRAMES;
     Rectangle sourceRecUkraine = { 0, 0, (float)buttonUkraine.width, 100 };
     Rectangle btnBoundsUkraine = { 480, 675, (float)buttonUkraine.width, (float)frameHeightUkraine };
-
-    int btnStateFrance = 0;
-
-    Vector2 mousePoint = { 0.0f, 0.0f };
-
-    mousePoint = GetMousePosition();
-
-        if (CheckCollisionPointRec(mousePoint, btnBoundsFrance))
+    buttons.push_back(btnBoundsUkraine);
+    
+    for (size_t i = 0; i < buttons.size(); i++)
+    {
+        if (CheckCollisionPointRec(GetMousePosition(), buttons[i]) && IsMouseButtonPressed(0))
         {
+            drawRec = true;
+            buttonToDisplay = i;
         }
-        else btnStateFrance = 0;
-
-    sourceRecFrance.y = btnStateFrance*frameHeightFrance;
-
+    }
+    
     BeginDrawing();
 
         ClearBackground(WHITE);
 
         DrawTexture(map, 0, 0, WHITE);
 
-        DrawTextureRec(buttonAustria, sourceRecAustria, {btnBoundsAustria.x, btnBoundsAustria.y }, WHITE); 
-        DrawTextureRec(buttonBelgium, sourceRecBelgium, {btnBoundsBelgium.x, btnBoundsBelgium.y }, WHITE); 
+        if (drawRec)
+        {
+            DrawRectangleRec(buttons[buttonToDisplay], GREEN);
+            // shte vikna kliment da te bie haha ne nqma haha ne
+        }
+
+        DrawTextureRec(buttonAustria, sourceRecAustria, {btnBoundsAustria.x, btnBoundsAustria.y }, WHITE);
+        DrawTextureRec(buttonBelgium, sourceRecBelgium, {btnBoundsBelgium.x, btnBoundsBelgium.y }, WHITE);
         DrawTextureRec(buttonBulgaria, sourceRecBulgaria, {btnBoundsBulgaria.x, btnBoundsBulgaria.y }, WHITE); 
         DrawTextureRec(buttonDenmark, sourceRecDenmark, {btnBoundsDenmark.x, btnBoundsDenmark.y }, WHITE); 
         DrawTextureRec(buttonFrance, sourceRecFrance, {btnBoundsFrance.x, btnBoundsFrance.y }, WHITE);
